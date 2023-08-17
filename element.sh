1a8aa77bcf69481e085e99afeb07311c13380b74
3v-mb <139736780+3v-mb@users.noreply.github.com>
2023-08-17 22:26:51 +0530

    Update element.sh

--- element.sh
+++ element.sh
@@ -8,18 +8,20 @@ then
 fi
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-case INPUT in
-[0-9]+)
-    ATOMIC_NUMBER=INPUT
-    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
-    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
-    TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-    TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
-    MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-    BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-    MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-  ;;
-[A-Z][a-z])
+if [[ input =~ [0-9]+ ]]
+then
+  ATOMIC_NUMBER=INPUT
+  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
+  NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
+  TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
+  MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+fi
+
+if [[ input =~ [A-Z][a-z] ]]
+then
   SYMBOL=INPUT
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$SYMBOL'")
   NAME=$($PSQL "SELECT name FROM elements WHERE symbol = '$SYMBOL'")
@@ -28,9 +30,10 @@ case INPUT in
   MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
   BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
   MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-  ;;
+fi
   
-[A-Za-z]+)
+if [[ input =~ [A-Za-z]+ ]]
+then
   NAME=INPUT
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$NAME'")
   SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
@@ -39,9 +42,9 @@ case INPUT in
   MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
   BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
   MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-  ;;
-*) echo -e "I could not find that element in the database."
-;;
-esac
+
+else
+  echo -e "I could not find that element in the database."
+fi
 
 echo $NAME $SYMBOL $TYPE $MP $BP $MASS
